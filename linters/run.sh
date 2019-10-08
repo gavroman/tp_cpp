be8adad720ee139f740a7cbd40270ad3c2bb18de
gavroman <gavroman1998@gmail.com>
2019-10-08 23:48:52 +0300

    Travis try3

--- linters/run.sh
+++ linters/run.sh
@@ -6,7 +6,7 @@ function print_header() {
 
 #print_header "RUN cppcheck"
 
-#./linters/cppcheck/cppcheck project --language=c --enable=all --error-exitcode=1 -I project/include --suppress=missingIncludeSystem
+./linters/cppcheck/cppcheck project --language=c --enable=all --error-exitcode=1 -I project/include --suppress=missingIncludeSystem
 
 print_header "RUN cpplint.py"
 python2.7 ./linters/cpplint/cpplint.py --extensions=c project/include/* project/src/*
